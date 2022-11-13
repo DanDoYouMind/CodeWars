ff928b4d844178046d1f4fa87b743f8f0e0a9cbf
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-11-13 06:52:09 -0800

    added new codewar solutions

---
+++ 8kyu/Well of Ideas- Easy Version.js
@@ -0,0 +1,16 @@
+/* INSTRUCTIONS: 
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.*/
+
+// SOLUTION:
+
+function well(x){
+    let counter = 0
+    x.forEach((book)=>{
+             if(book == 'good'){
+      counter+= 1
+    }})
+    return counter == 0 ? 'Fail!' : counter == 1 || counter == 2 ? 'Publish!' : 'I smell a series!'
+    }
