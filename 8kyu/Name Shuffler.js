4da35343922185fd0779ef95105042329d53071c
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-11-16 20:15:45 -0800

    added new codewars

---
+++ 8kyu/Name Shuffler.js
@@ -0,0 +1,12 @@
+/* INSTRUCTIONS: 
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"*/
+
+// SOLUTION:
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
