175463db218757c4c430da8524a256f4d9f5865d
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-11-23 07:15:44 -0800

    added new codewar solution

---
+++ 8kyu/Remove duplicates from list.js
@@ -0,0 +1,8 @@
+/* INSTRUCTIONS: 
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+The order of the sequence has to stay the same.*/
+
+//SOLUTION:
+
+let distinct = a => Array.from(new Set(a))
+
