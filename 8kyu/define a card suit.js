408bc2d0aab1a6da53047a2c47e79736cbcaedd5
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-12-03 22:05:13 -0800

    added new codewar solution

---
+++ 8kyu/define a card suit.js
@@ -0,0 +1,25 @@
+/* INSTRUCTIONS:
+
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades' */
+
+//SOLUTION:
+
+function defineSuit(card) {
+    let suit = card.slice(-1);
+    switch(suit) {
+      case '♣':
+        return 'clubs';
+      case '♦':
+        return 'diamonds';
+      case '♥':
+        return 'hearts';
+      case '♠':
+        return 'spades';
+    }}
