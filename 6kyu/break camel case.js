7a3a0559079543ef2dcfbefe2978514f761d9019
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-12-07 20:03:34 -0800

    added new codewar solution

---
+++ 6kyu/break camel case.js
@@ -0,0 +1,21 @@
+/* INSTRUCTIONS: 
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  "" */
+
+// SOLUTION:
+
+function solution(string) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i].toUpperCase()) {
+            result += ' ' + string[i];
+        } else {
+            result += string[i];
+        }
+    }
+    return result;
+}
