a3e17979ad19df094e598452f4a5c25a2dded969
Daniel Wilk <wilk.a.daniel@gmail.com>
2022-12-08 22:30:57 -0800

    added new codewar solution

--- 7kyu/Square Every Digit.js
+++ 7kyu/Square Every Digit.js
@@ -4,12 +4,10 @@ Welcome. In this kata, you are asked to square every digit of a number and conca
 
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
-Note: The function accepts an integer and returns an integer*/
+Note: The function accepts an integer and returns an integer */
 
-//SOLTUION:
+//SOLUTION:
 
 function squareDigits(num){
     return Number(String(num).split('').map(x => (+x)**2).join(''))
   }
-
-  
